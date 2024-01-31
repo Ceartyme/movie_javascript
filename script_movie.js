8f1062ea1e08d703fdd69870a39e01d2ddf9979b
Ceartyme <csndz1505@gmail.com>
2024-01-31 23:58:07 +0100

    Feat[movie] : création de la structure de la page movie.html

---
+++ script_movie.js
@@ -0,0 +1,16 @@
+const id=window.location.search.split("=")[1]
+
+/* 
+- titre
+- type
+- release
+- runtime
+- genre
+- plot (puis plot long avec un bouton)
+- Awards
+- poster
+- ratings
+- total seasons (if Type=series)
+- director 
+- actors
+*/
